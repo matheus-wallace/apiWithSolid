cc603d4ff729eb43794a1c94534264f52366afa9
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-03-25 18:33:00 -0300

    feat: first controller to registrer a new user

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
-prisma.user.create({
-  data: {
-    name: 'matheus',
-    email: 'matheus@test.com',
-  },
+app.post('/users', async (request, reply) => {
+  const resgisterBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = resgisterBodySchema.parse(request.body)
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+  reply.status(201).send()
 })
