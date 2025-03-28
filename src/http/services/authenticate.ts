a233884262636d02ba2bc2360819f84cfac88a9a
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-03-27 21:11:34 -0300

    feat: add new verification to check if password and email matches

---
+++ src/http/services/authenticate.ts
@@ -0,0 +1,38 @@
+import { compare } from 'bcryptjs'
+import { UsersRepository } from '../../repositories/prisma/users-repository'
+import { InvalidCredentialsError } from './erros/invalid-credentials-error'
+import { User } from '@prisma/client'
+
+interface AuthenticateServiceRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateServiceResponse {
+  user: User
+}
+
+export class AuthenticateService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateServiceRequest): Promise<AuthenticateServiceResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMathes = await compare(password, user.password_hash)
+
+    if (!doesPasswordMathes) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
