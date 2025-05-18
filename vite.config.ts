de0e92af1b2fc1f2c863623fd97d8635a3d83271
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-05-18 14:46:16 -0300

    feature: add enviroment tests for unit and e2e tests

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,18 @@ import tsconfigPaths from 'vitest-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    dir: 'src',
+    workspace: [
+      { extends: true, test: { name: 'unit', dir: 'src/services' } },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment: './prisma/vitest-envrionment/prisma-test-environment.ts',
+        },
+      },
+    ],
+  },
 })
